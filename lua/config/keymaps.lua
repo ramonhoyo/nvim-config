f3cbfec8edb59f3d2ae3851b35dd48cb19238273
Ramón Hoyo <rahoyo@outlook.com>
2024-09-17 22:25:41 -0400

    updat

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,24 @@
 
 vim.keymap.set("n", "<leader><F3>", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<leader><F2>", vim.cmd.Neotree)
+
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+vim.keymap.set("n", "<F10>", function()
+  require("dap").continue()
+end)
+
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end)
+vim.keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end)
+vim.keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end)
+
+vim.keymap.set("n", "<Leader>q", function()
+  require("dap").toggle_breakpoint()
+end)
